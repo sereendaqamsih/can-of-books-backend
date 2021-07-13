8badb772e3a796fff00d482ae43dbb7848baf38a
sereendaqamsih <sereen.aldaqamsih@gmail.com>
2021-07-13 18:59:20 +0300

    save lab 13

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/user', { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
 app.use(cors());
+
+app.use(express.json());
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -18,18 +21,63 @@ const PORT = process.env.PORT;
 app.get('/test', (request, response) => {
   response.send('done')
 })
-app.get('/book',getUserdata);
+app.get('/books',getUserdata);
 // http://localhost:3001/book?userEmail=sereen.aldaqamsih@gmail.com
+app.post('/book',addBookHandler);
+app.delete('/deleteBook/:bookId',deleteBookHandler);
 
 function getUserdata(req,res){
   let userEmail = req.query.userEmail;
-
   myUserModel.find({email:userEmail},function(error,userDAta){
     console.log(userDAta);
     if (error){res.send(error);}
     else {res.send(userDAta[0].book);}
   
-  })
+  })}
+
+  function addBookHandler (req,res) {
+
+  let {addTitle,addDescription,addLink,addStatus,email}=req.body;
+
+  myUserModel.find({email:email},(error,bookData) => {
+    if (error)  { res.send('no data',error) }
+    else {
+      console.log('before',bookData);
+    bookData[0].book.push({
+      name:addTitle,
+      status:addStatus,
+      description:addDescription,
+      img:addLink,
+
+    })
+  console.log('after',bookData[0]);
+  bookData[0].save();
+  res.send(bookData[0].book)
+ } 
+})
 }
 
+function deleteBookHandler (req,res) {
+  // console.log(req.params)
+  // console.log(req.query)
+  let index = Number(req.params.bookId);
+  // console.log(index)
+  let email = req.query.email;
+  myUserModel.find({email:email},(error,bookData)=>{
+      if(error) {res.send('cant find user')}
+      else{
+        //  console.log('before deleting',bookData[0].book)
+
+         let newBookData = bookData[0].book.filter((book,idx)=>{
+             if(idx !== index) {return book}
+          // return idx!==index
+         })
+         bookData[0].book=newBookData
+         console.log('after deleting',bookData[0].book)
+         bookData[0].save();
+         res.send(bookData[0].book)
+      }
+
+  })
+}
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
